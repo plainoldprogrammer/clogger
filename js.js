6acfadf7c721683f95ed03f804514ec6d02338fe
unknown <plainoldprogrammer@gmail.com>
2020-07-20 02:04:20 -0500

    defining the global clogger object

--- js.js
+++ js.js
@@ -1,3 +1,5 @@
+let clogger = new Object();
+
 let info = function(message) {
   console.log('%c%s', 'color: blue', message);
 }
@@ -9,3 +11,7 @@ let error = function(message) {
 let warning = function(message) {
   console.log('%c%s', 'color: orange', message);
 }
+
+clogger.info = info;
+clogger.error = error;
+clogger.warning = warning;
