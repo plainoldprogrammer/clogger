3fb7bea7d57f5a5d525cc412731a91ac6589ecc6
unknown <plainoldprogrammer@gmail.com>
2020-07-20 02:07:09 -0500

    transforming the logging functions to methods

--- js.js
+++ js.js
@@ -1,17 +1,13 @@
 let clogger = new Object();
 
-let info = function(message) {
+clogger.info = function(message) {
   console.log('%c%s', 'color: blue', message);
 }
 
-let error = function(message) {
+clogger.error = function(message) {
   console.log('%c%s', 'color: red', message);
 }
 
-let warning = function(message) {
+clogger.warning = function(message) {
   console.log('%c%s', 'color: orange', message);
 }
-
-clogger.info = info;
-clogger.error = error;
-clogger.warning = warning;
